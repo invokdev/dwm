21a0098aade856ce28a2278c16fa5a1bd05a8daa
invokdev <kdempsey@invokdev.com>
2021-06-10 11:43:17 -0400

    updated terminals

--- config.h
+++ config.h
@@ -60,10 +60,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *chrome[] = { "google-chrome-stable", NULL };
 static const char *monkey[] = { "seamonkey", NULL };
-static const char *xterm[] = { "xfce4-terminal", NULL };
+static const char *sterm[] = { "st", NULL };
 static const char *vscode[] = { "code", NULL };
 
 static Key keys[] = {
@@ -72,7 +72,7 @@ static Key keys[] = {
 	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_g,	   spawn,	   {.v = chrome } },
 	{ MODKEY,			XK_a,	   spawn,	   {.v = monkey } },
-	{ MODKEY,			XK_x,	   spawn,	   {.v = xterm } },
+	{ MODKEY,			XK_x,	   spawn,	   {.v = sterm } },
 	{ MODKEY,			XK_c,	   spawn,	   {.v = vscode } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
