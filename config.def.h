582baeeaca1acd37b4e41bfbe89eeee4b2235a63
invokdev <kdempsey@invokdev.com>
2021-07-19 18:55:54 -0400

    updated color scheme

--- config.def.h
+++ config.def.h
@@ -7,9 +7,9 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=16" };
 static const char dmenufont[]       = "monospace:size=16";
-static const char col_gray1[]       = "#3E3758";
+static const char col_gray1[]       = "#FDFBFC";
 static const char col_gray2[]       = "#DA6A38";
-static const char col_gray3[]       = "#E58A3C";
+static const char col_gray3[]       = "#2D2D2D";
 static const char col_gray4[]       = "#AD8605";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
